032a03f8af2c94e87dce00738d489bc6ef61b0cc
ValkyRiE2117 <aritzagk@gmail.com>
2024-10-01 15:52:38 +0700

    10-1-2024

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+</head>
+<body>
+    <x-header></x-header>
+    <div class="bg-gray-100">
+        {{$slot}}
+    </div>
+    <x-footer></x-footer>
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    50: "#f1f7fe",
+                    100: "#e2edfc",
+                    200: "#bfdaf8",
+                    300: "#87bdf2",
+                    400: "#479ae9",
+                    500: "#1f7ed8",
+                    600: "#1161b8",
+                    700: "#0f4c92",
+                    800: "#11437b",
+                    900: "#143966",
+                    950: "#0d2444",
+                },
+            },
+        },
+    },
+    plugins: [require("flowbite/plugin")],
+};

752fb37162eafa5646184d3336189ab9cdea01bd
ValkyRiE2117 <aritzagk@gmail.com>
2024-10-02 10:44:05 +0700

    2-10-2024

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,45 +1,23 @@
 <footer class="bg-gray-800">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
-          <div class="mb-6 md:mb-0">
-            <a href="#" class="flex items-center">
-                <img src="{{asset('img/logodti-dark.png')}}" class="mr-3 h-24" alt="D3-TI Logo" />
-            </a>
+          <div class="mb-6 md:mb-0 border">
+                <a href="#" class="flex items-center">
+                    <img src="{{asset('img/logonondti.png')}}" class="mr-3 h-24" alt="D3-TI Logo" />
+                </a>
           </div>
-          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-              <div>
-                  <h2 class="mb-6 text-sm font-semibold uppercase text-white">Resources</h2>
-                  <ul class=" text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
-                      </li>
-                      <li>
-                          <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
-                      </li>
-                  </ul>
-              </div>
-              <div>
-                  <h2 class="mb-6 text-sm font-semibold  uppercase text-white">Follow us</h2>
-                  <ul class=" text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Github</a>
-                      </li>
-                      <li>
-                          <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
-                      </li>
-                  </ul>
-              </div>
-              <div>
-                  <h2 class="mb-6 text-sm font-semibold  uppercase text-white">Legal</h2>
-                  <ul class=" text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="#" class="hover:underline">Privacy Policy</a>
-                      </li>
-                      <li>
-                          <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                      </li>
-                  </ul>
-              </div>
+          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-1 border">
+            <div>
+                <h2 class="mb-6 text-sm font-semibold  uppercase text-white">Legal</h2>
+                    <ul class=" text-gray-400 font-medium">
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline">Terms &amp; Conditions</a>
+                        </li>
+                    </ul>
+                </div>
           </div>
       </div>
       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-layout >
     <section class="">
         <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-8 lg:px-6">
-            <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400 ">
+            <div class="font-light text-gray-500 sm:text-lg">
                 <h1 class="mb-4 lg:text-5xl text-4xl  tracking-tight font-extrabold text-primary-700 text-center lg:text-start">Program Studi Teknik Informatika - D3</h1>
                 <p class="mb-4 text-justify">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
             </div>
@@ -17,19 +17,19 @@
             <div class="grid max-w-screen-lg gap-8 mx-auto text-primary-700 sm:grid-cols-4">
                 <div class="flex flex-col items-center justify-center">
                     <dt class="text-4xl mb-2 md:text-5xl  font-extrabold">Unggul</dt>
-                    <dd class="font-light text-gray-500 dark:text-gray-400">Akreditasi</dd>
+                    <dd class="font-light text-gray-500">Akreditasi</dd>
                 </div>
                 <div class="flex flex-col items-center justify-center">
                     <dt class="text-4xl mb-0 md:mb-2 md:text-5xl font-extrabold">50+</dt>
-                    <dd class="font-light text-gray-500 dark:text-gray-400">Mahasiswa</dd>
+                    <dd class="font-light text-gray-500">Mahasiswa</dd>
                 </div>
                 <div class="flex flex-col items-center justify-center">
                     <dt class="text-4xl mb-0 md:mb-2 md:text-5xl  font-extrabold">6</dt>
-                    <dd class="font-light text-gray-500 dark:text-gray-400">Tenaga Pengajar</dd>
+                    <dd class="font-light text-gray-500">Tenaga Pengajar</dd>
                 </div>
                 <div class="flex flex-col items-center justify-center">
                     <dt class="text-4xl mb-0 md:mb-2 md:text-5xl  font-extrabold">200+</dt>
-                    <dd class="font-light text-gray-500 dark:text-gray-400">Lulusan</dd>
+                    <dd class="font-light text-gray-500">Lulusan</dd>
                 </div>
             </div>
         </div>
@@ -86,10 +86,10 @@
         </div>
       </section>
       <section class="">
-        <div class=" mb-8">
+        <div class="mb-8">
             <h2 class=" text-3xl tracking-tight font-extrabold text-gray-700 text-center">Sambutan Ketua Program Studi Teknik Infromatika - D3</h2>
         </div>
-        <div class="gap-0 items-start py-8 px-4 mx-auto max-w-screen-lg xl:gap-16 md:grid md:grid-cols-12 sm:py-16 lg:px-6">
+        <div class="gap-0 items-start py-8 px-4 mx-auto max-w-screen-lg xl:gap-16 md:grid md:grid-cols-12 sm:py-16 lg:px-6"  >
             <img class="w-2/4 mx-auto md:w-4/5 col-span-4 md:ml-0" src="{{ asset('img/kaprodi.jpg') }}" alt="dashboard image">
             <div class="mt-4 md:mt-0 col-span-8">
                 <figure class="max-w-screen-md h-full mx-auto">
@@ -118,76 +118,77 @@
                 <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md">
                     <img class="h-auto max-w-full rounded-lg mb-6" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="image description">
                     <div class="flex justify-between items-center mb-5 text-gray-500">
-                        <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                            <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
+                        <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded ">
+                            
                             Tutorial
                         </span>
                         <span class="text-sm">14 days ago</span>
                     </div>
-                    <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 e"><a href="#">How to quickly deploy a static website</a></h2>
+                    <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 e"><a href="#">How to quickly deploy a static website</a></h3>
                     <p class="mb-5 font-light text-gray-500 ">Static websites are now used to bootstrap lots of websites and are becoming the basis</p>
-                    {{-- <div class="flex justify-between items-center">
-                        <div class="flex items-center space-x-4">
-                            <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
-                            <span class="font-medium dark:text-white">
-                                Jese Leos
-                            </span>
-                        </div>
-                        <a href="#" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                            Read more
-                            <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        </a>
-                    </div> --}}
                 </article> 
                 <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md">
                     <img class="h-auto max-w-full rounded-lg mb-6" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="image description">
                     <div class="flex justify-between items-center mb-5 text-gray-500">
-                        <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                            <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
+                        <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded ">
+                            
                             Tutorial
                         </span>
                         <span class="text-sm">14 days ago</span>
                     </div>
-                    <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 e"><a href="#">How to quickly deploy a static website</a></h2>
+                    <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 e"><a href="#">How to quickly deploy a static website</a></h3>
                     <p class="mb-5 font-light text-gray-500 ">Static websites are now used to bootstrap lots of websites and are becoming the basis</p>
-                    {{-- <div class="flex justify-between items-center">
-                        <div class="flex items-center space-x-4">
-                            <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
-                            <span class="font-medium dark:text-white">
-                                Jese Leos
-                            </span>
-                        </div>
-                        <a href="#" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                            Read more
-                            <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        </a>
-                    </div> --}}
+                    
                 </article> 
                 <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md">
                     <img class="h-auto max-w-full rounded-lg mb-6" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="image description">
                     <div class="flex justify-between items-center mb-5 text-gray-500">
-                        <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                            <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
+                        <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded ">
+                            
                             Tutorial
                         </span>
                         <span class="text-sm">14 days ago</span>
                     </div>
-                    <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 e"><a href="#">How to quickly deploy a static website</a></h2>
+                    <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 e"><a href="#">How to quickly deploy a static website</a></h3>
                     <p class="mb-5 font-light text-gray-500 ">Static websites are now used to bootstrap lots of websites and are becoming the basis</p>
-                    {{-- <div class="flex justify-between items-center">
-                        <div class="flex items-center space-x-4">
-                            <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
-                            <span class="font-medium dark:text-white">
-                                Jese Leos
-                            </span>
-                        </div>
-                        <a href="#" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                            Read more
-                            <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        </a>
-                    </div> --}}
+                    
                 </article> 
             </div>  
         </div>
-      </section>
+    </section>
+    <section class="">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:px-6">
+            <div class="mx-auto max-w-screen-xl text-center lg:mb-16 mb-8">
+                <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-700 ">Program Kerja Himpunan Mahasiswa Diploma Teknik Informatika</h2>
+                <p class="font-light text-gray-500 sm:text-xl"></p>
+            </div> 
+            <div class="grid gap-8 lg:grid-cols-3">
+                <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md">
+                    <img class="h-auto max-w-full rounded-lg mb-2" src="{{asset('img/WLP.jpg')}}" alt="image description">
+                    <div class="flex justify-between items-center mb-2 text-gray-500">
+                        {{-- <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded ">
+                            
+                            Tutorial
+                        </span> --}}
+                        <span class="text-sm"></span>
+                    </div>
+                    <h3 class=" text-2xl font-bold tracking-tight text-gray-900 e"><a href="#">How to quickly deploy a static website</a></h3>
+                
+                </article> 
+                <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md">
+                    <img class="h-auto max-w-full rounded-lg mb-2" src="{{asset('img/uiux.jpg')}}" alt="image description">
+                    <div class="flex justify-between items-center mb-2 text-gray-500">
+                        {{-- <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded ">
+                            
+                            Tutorial
+                        </span>
+                        <span class="text-sm"></span> --}}
+                    </div>
+                    <h3 class=" text-2xl font-bold tracking-tight text-gray-900"><a href="#">How to quickly deploy a static website</a></h3>
+                    
+                </article> 
+                
+            </div>  
+        </div>
+    </section>
 </x-layout>
